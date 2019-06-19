bef99e334924faf647c82fadcf67f7e5035df0bc
jeevcoding <jeevyaballal282@gmail.com>
2019-06-20 00:00:36 +0530

    qr code reader base(with url integration)

--- app/src/main/java/com/jeevcode/barcodeqrcodescanner/MainActivity.java
+++ app/src/main/java/com/jeevcode/barcodeqrcodescanner/MainActivity.java
@@ -3,10 +3,15 @@ package com.jeevcode.barcodeqrcodescanner;
 
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -38,6 +43,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btnScanBarcode:
                 startActivity(new Intent(MainActivity.this, ScannedBarcodeActivity.class));
                 break;
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+
+            case R.id.pp:
+                startActivity(new Intent("android.intent.action.VIEW", Uri.parse("https://www.termsfeed.com/privacy-policy/139eccf173f5e239085d2eed4a09fdf7")));
+                Toast.makeText(getApplicationContext(),"selected privacy policy",Toast.LENGTH_SHORT).show();
+                return true;
+
+                default:
+                    return super.onOptionsItemSelected(item);
+
         }
 
     }
